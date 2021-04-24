b3d3949dfcc21869fbf5487efc77ba9ac79c0459
pablo <paulandrewjeffrey@gmail.com>
2021-04-24 15:48:25 -0700

    one more commit, bro

--- server.js
+++ server.js
@@ -1,14 +1,59 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
-const dotenv = require('dotenv').config()
 const connection = require('./config/connection')
+const cTable = require("console.table");
 
-connection.connect((err) => {
-    if (err) throw err
-    firstQuestion()
-});
-
-const firstQuestion = () => {
-    console.log('FUCK')
+const start = () => {
+  inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View departments',
+            'View employees',
+            'View roles',
+            'Add a department',
+            'Add an employee',
+            'Add a role',
+            'Change an employee\'s role',
+            'Quit'
+        ],
+        loop: false
+  })
+  .then((answwer) => {
+      switch(answwer.action) {
+        case 'View departments':
+            viewDepartments();
+            break;
+        case 'View employees':
+            viewEmployees();
+            break;
+        case 'View roles':
+            viewRoles();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Change an employee\'s role':
+            changeRole();
+            break;
+        case 'Quit':
+            connection.end(console.log('Adios!'));
+            break;
+      }
+  })
 }
+
+
+
+connection.connect((error) => {
+    if (error) throw error;
+    start();
+});
